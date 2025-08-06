76a34e1b7123268496d0e6efa344b654976064a2
Kaiki <k19375m@gmail.com>
2025-08-05 21:49:50 -0300

    Adição de imagem e ivremento no HTML

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,9 @@
-#recipes/urls.py
-
-from django.urls import path
-from . import views
+# recipes/urls.py
+from django.urls import path # Importa a função 'path' que define URLs.
+from . import views # Importa o arquivo views.py do próprio app.
 
 urlpatterns = [
-    path('', views.home, name='recipes_home'),
+path('', views.recipe_list, name='recipe_list'), 
+path('<int:pk>/', views.recipe_detail, name='recipe_detail')
+
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse
+from .models import Recipe
 
-def home(request):
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'recipes/recipes_list.html', {'recipes': recipes})
 
-    return HttpResponse("<h1>Bem-vindo à página de Receitas!</h1>")
+def recipe_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
